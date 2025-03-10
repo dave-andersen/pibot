fa6d3297af9900903bd94f4ed756a4a150511b2a
David G. Andersen <dave.andersen@gmail.com>
2025-03-10 01:52:06 +0000

    add streaming mode

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
-use atrium_api::app::bsky::feed::post::RecordData;
+use anyhow::anyhow;
+use atrium_api::app::bsky::feed::post::{RecordData, ReplyRefData};
+use atrium_api::app::bsky::richtext::facet::MainFeaturesItem::Mention;
+use atrium_api::com::atproto::repo::strong_ref;
+use atrium_api::types::Union;
 use atrium_api::types::string::Datetime;
+use atrium_api::{record::KnownRecord::AppBskyFeedPost, types::string};
 use bsky_sdk::BskyAgent;
 use bsky_sdk::rich_text::RichText;
 use clap::{Parser, Subcommand};
 use fastrand;
+use jetstream_oxide::{
+    DefaultJetstreamEndpoints, JetstreamCompression, JetstreamConfig, JetstreamConnector,
+    events::{JetstreamEvent::Commit, commit::CommitEvent},
+};
 use reqwest;
 use serde::Deserialize;
 use serde_json::Value;
@@ -12,7 +21,7 @@ use std::fs::File;
 use std::io::Read;
 use tokio;
 
-fn read_credentials() -> Result<(String, String), Box<dyn std::error::Error>> {
+fn read_credentials() -> Result<(String, String, String), Box<dyn std::error::Error>> {
     let home_dir = env::var("HOME")?;
     let file_path = format!("{}/.pibot_login.json", home_dir);
     let mut file = File::open(file_path)?;
@@ -27,7 +36,11 @@ fn read_credentials() -> Result<(String, String), Box<dyn std::error::Error>> {
         .as_str()
         .ok_or("Password not found")?
         .to_string();
-    Ok((username, password))
+    let watch_did = json["watch_did"]
+        .as_str()
+        .ok_or("Watch DID not found")?
+        .to_string();
+    Ok((username, password, watch_did))
 }
 
 #[allow(dead_code)]
@@ -48,6 +61,13 @@ struct ResultEntry {
     db: String,
     da: String,
     c: u32,
+}
+
+async fn search_pi_str(number: &str) -> Result<PiSearchResult, Box<dyn std::error::Error>> {
+    let url = format!("https://www.angio.net/newpi/piquery?q={}", number);
+    let response = reqwest::get(&url).await?.text().await?;
+    let result: PiSearchResult = serde_json::from_str(&response)?;
+    Ok(result)
 }
 
 async fn search_pi(number: u32) -> Result<PiSearchResult, Box<dyn std::error::Error>> {
@@ -73,11 +93,7 @@ fn get_today_date() -> String {
     about = "Posts Pi search results to Bsky"
 )]
 struct Cli {
-    #[arg(
-        long,
-        help = "Be a BlueSky Bot. Commands: random, today, stream"
-    )]
-
+    #[arg(long, help = "Be a BlueSky Bot. Commands: random, today, stream")]
     #[arg(
         long,
         short = 'n',
@@ -85,17 +101,21 @@ struct Cli {
     )]
     dry_run: bool,
     #[command(subcommand)]
-    command: Commands
+    command: Commands,
 }
 
 #[derive(Subcommand, PartialEq)]
 enum Commands {
     Random,
     Today,
-    Stream
+    Stream,
 }
 
-async fn post_to_bsky(agent: &BskyAgent, text: &str, dry_run: bool) -> Result<(), Box<dyn std::error::Error>> {
+async fn post_to_bsky(
+    agent: &BskyAgent,
+    text: &str,
+    dry_run: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
     let rt = RichText::new_with_detect_facets(text).await?;
     let record_data = RecordData {
         created_at: Datetime::now(),
@@ -110,31 +130,201 @@ async fn post_to_bsky(agent: &BskyAgent, text: &str, dry_run: bool) -> Result<()
     };
 
     if dry_run {
-        println!("Dry run mode: The post content would be:\n{:?}", record_data);
+        println!(
+            "Dry run mode: The post content would be:\n{:?}",
+            record_data
+        );
     } else {
         agent.create_record(record_data).await?;
     }
     Ok(())
 }
 
-
-async fn streaming_mode(username: String, password: String, dry_run: bool) -> Result<(), Box<dyn std::error::Error>> {
+async fn streaming_mode(
+    username: String,
+    password: String,
+    watch_did: &str,
+    dry_run: bool,
+) -> Result<(), Box<dyn std::error::Error>> {
     let agent = BskyAgent::builder().build().await?;
-    let _session = agent.login(&username, &password).await?;
+    let session = agent.login(&username, &password).await?;
     println!("Streaming mode!");
-    println!("As you can tell, this doesn't exist yet.");
+    let target_did = atrium_api::types::string::Did::new(watch_did.into())?;
+    let nsid = jetstream_oxide::exports::Nsid::new("app.bsky.feed.post".into()).unwrap();
+    let config = JetstreamConfig {
+        wanted_collections: vec![nsid.into()],
+        endpoint: DefaultJetstreamEndpoints::USEastTwo.into(),
+        compression: JetstreamCompression::Zstd,
+        ..Default::default()
+    };
+
+    let jetstream = JetstreamConnector::new(config).unwrap();
+    let receiver = jetstream.connect().await?;
+
+    while let Ok(event) = receiver.recv_async().await {
+        match event {
+            Commit(CommitEvent::Create { info, commit, .. }) => {
+                let event_did = info.did.to_string();
+                if let AppBskyFeedPost(record) = &commit.record {
+                    let mut matches = false;
+                    if let Some(facets) = &record.facets {
+                        for facet in facets {
+                            for feature in &facet.data.features {
+                                if let Union::Refs(Mention(m)) = feature {
+                                    if m.data.did == target_did {
+                                        matches = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if matches {
+                        if !dry_run {
+                            handle_message(&agent, &event_did, &commit).await;
+                        } else {
+                            println!("Dry run mode: Would handle message from {}", event_did);
+                            println!("Message to post: {:#?}", record);
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
     println!("Exiting streaming mode!");
     Ok(())
+}
+
+pub fn extract_number(text: &str) -> Option<String> {
+    let re = regex::Regex::new(r"@pisearch[^\d]*(\d[\d\-]*)").unwrap();
+    re.captures(text)
+        .and_then(|cap| cap.get(1))
+        .map(|m| m.as_str().to_string())
+        .and_then(|s| {
+            let number = s.replace("-", "");
+            if number.is_empty() {
+                None
+            } else {
+                Some(number)
+            }
+        })
+}
+
+pub async fn do_pisearch(text: &str) -> anyhow::Result<String> {
+    let number = match extract_number(text) {
+        Some(num) => num,
+        None => {
+            return Err(anyhow!("No number found"));
+        }
+    };
+    println!("Searching for: {}", number);
+
+    let search_result = match search_pi_str(&number).await {
+        Ok(result) => result,
+        Err(e) => {
+            println!("Error searching: {e}");
+            return Err(anyhow!("Error searching: {e}"));
+        }
+    };
+    if search_result.status == "error" {
+        return Err(anyhow!("Error searching pi"));
+    }
+    match search_result.r.first() {
+        None =>
+            Ok(format!("Sorry, I couldn't find {number} in the first 200m digits of Pi. It's me, not you; every number should be in Pi if I had more.").into()),
+        Some(entry) => {
+            if entry.status == "notfound" {
+                Ok(format!("Sorry, I couldn't find {number} in the first 200m digits of Pi. It's me, not you; every number should be in Pi if I had more.").into())
+            } else {
+            Ok(format!(
+        "I found {} at position {}. It appears {} times in the first 200 million digits of pi. Thanks for searching!\n\nFind all the #pi you can eat at https://angio.net/pi/",
+        number,
+        search_result.r.first().map_or(0, |entry| entry.p),
+        search_result.r.len()
+    ))
+        }
+    }
+}
+}
+
+pub async fn handle_message(
+    agent: &BskyAgent,
+    did: &str,
+    commit: &jetstream_oxide::events::commit::CommitData,
+) {
+    use bsky_sdk::rich_text::RichText;
+
+    if let AppBskyFeedPost(record) = &commit.record {
+        let rt = RichText::new_with_detect_facets(&record.text).await;
+        // Ugh - have to figure out if the post itself was a reply. If it was,
+        // get the "parent" field out of it and propagate it here.
+        // Second ugh - we need to craft a URI for the post since it doesn't
+        // seem to be included in the data.
+        // at://<did>/app.bsky.feed.post/<rkey>
+        let uri = format!(
+            "at://{}/{}/{}",
+            did,
+            commit.info.collection.to_string(),
+            commit.info.rkey
+        );
+
+        let reply_text = match do_pisearch(&record.text).await {
+            Ok(text) => text,
+            Err(e) => {
+                println!("Error searching pi: {}", e);
+                return;
+            }
+        };
+
+        let root_data = match &record.reply {
+            Some(data) => {
+                println!("Found a reply ref: {data:?}");
+                strong_ref::MainData {
+                cid: data.root.cid.clone(),
+                uri: data.root.uri.clone(),
+            }},
+            _ => strong_ref::MainData {
+                cid: commit.cid.clone(),
+                uri: uri.clone(),
+            },
+        };
+
+        let record_data = RecordData {
+            created_at: Datetime::now(),
+            embed: None,
+            entities: None,
+            facets: None, // rt.facets,
+            labels: None,
+            langs: None,
+            reply: Some(
+                ReplyRefData {
+                    root: root_data.into(),
+                    parent: strong_ref::MainData {
+                        cid: commit.cid.clone(),
+                        uri: uri.into(),
+                    }
+                    .into(),
+                }
+                .into(),
+            ),
+            tags: Some(["#pi".to_string()].to_vec()),
+            text: reply_text.into(),
+        };
+
+        if let Err(e) = agent.create_record(record_data).await {
+            println!("Error creating record: {}", e);
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
-    let (username, password) = read_credentials()?;
+    let (username, password, watch_did) = read_credentials()?;
 
     if cli.command == Commands::Stream {
-        streaming_mode(username, password, cli.dry_run).await?;
+        streaming_mode(username, password, &watch_did, cli.dry_run).await?;
         return Ok(());
     }
     let number = match cli.command {
@@ -165,4 +355,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     post_to_bsky(&agent, &post_content, cli.dry_run).await?;
 
     Ok(())
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_number() {
+        assert_eq!(extract_number("@pisearch 12345"), Some("12345".to_string()));
+        assert_eq!(extract_number("@pisearch 67890"), Some("67890".to_string()));
+        assert_eq!(
+            extract_number("@pisearch 123-456-7890"),
+            Some("1234567890".to_string())
+        );
+        assert_eq!(extract_number("No number here"), None);
+        assert_eq!(extract_number("@pisearch"), None);
+        assert_eq!(extract_number("@pisearch -"), None);
+        assert_eq!(extract_number("@pisearch 123-"), Some("123".to_string()));
+        assert_eq!(
+            extract_number("Hey @pisearch.bsky.social where is 2025-03-09 in Pi?"),
+            Some("20250309".to_string())
+        );
+    }
 }
